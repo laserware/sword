35340886bba8907defb1076a2cf7b1555e8cf6eb
Mike Rourke <mike.w.rourke@gmail.com>
2024-05-08 12:14:58 -0700

    fix: update withSword function to prevent errors

--- src/withSword.ts
+++ src/withSword.ts
@@ -5,40 +5,37 @@ import { storeContextKey } from "./context";
 
 type Options = Parameters<typeof mount>[1];
 
+type MountOptions = ReturnType<typeof mount>;
+
 /**
- * Adds the specified Redux store to the specified Svelte component entry point
- * with the specified component options. This must be done in order to use the
- * {@link useDispatch}, {@link useSelector}, and {@link useStore} functions.
+ * Adds the specified Redux store to the specified Svelte component options.
+ * This must be done in order to use the {@link useDispatch}, {@link useSelector},
+ * and {@link useStore} functions.
  *
  * @template State Redux state definition.
  *
- * @param Component Svelte component entry point for the application.
- * @param options Svelte component constructor options.
  * @param store Redux store for the application.
+ * @param options Svelte component constructor options (if any).
  * @example
  * import { withSword } from "@laserware/sword";
+ * import { mount } from "svelte";
  *
  * import App from "./App.svelte";
  * import { createStore } from "./my-redux-store";
  *
  * const store = createStore();
  *
- * const app = withStore(
- *   store,
- *   App,
- *   { target: document.body },
- * );
+ * const app = mount(App, withSword(store, { target: document.body }));
  *
  * export default app();
  */
 export function withSword<State>(
   store: Store<State>,
-  Component: ComponentType,
-  options: Options,
-): () => SvelteComponent {
+  options: MountOptions,
+): MountOptions {
   const context = options.context ?? new Map();
 
   context.set(storeContextKey, store);
 
-  return () => mount(Component, options);
+  return { ...options, context };
 }
